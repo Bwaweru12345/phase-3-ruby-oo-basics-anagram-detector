5e5a52bdefac5e5fcc4137342ae0b090b47f17c6
Bwaweru12345 <bryanwaweru1076@example.com>
2023-05-19 18:47:28 +0300

    Complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+
+
+class Anagram 
+    attr_accessor :word, :match
+
+    def initialize (word)
+        @agram = word
+        @word = word.chars.sort
+        return @word
+
+    end
+
+    def match gram
+
+        @new = gram.select {|words| words.chars.sort == @word}
+        if @new 
+            return @new
+        else
+            return []
+        end
+    end
+end
+
+l = Anagram.new('mondya')
+l.match = (%w[tuesday monday, wednesday,friday])
